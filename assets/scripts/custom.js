1b222dd4addbfed645d24c5c6791fbfe382c6337
vosslab <vosslab@users.noreply.github.com>
2025-02-09 20:00:24 +0000

    deploy: e0e5eca4be0bee1426a42cc8959d2173473e7408

---
+++ assets/scripts/custom.js
@@ -0,0 +1,9 @@
+// Function to handle the file download
+function downloadFile(filename) {
+    const link = document.createElement('a');
+    link.href = filename;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
